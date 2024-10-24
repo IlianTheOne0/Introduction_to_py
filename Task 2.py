ff5479fd638aa0542c40eac5382e7e6b8508c007
Ilian Shchepinskyi <iliantheone@gmail.com>
2024-10-25 01:01:01 +0300

    Completed Task 2

---
+++ Task 2.py
@@ -0,0 +1,8 @@
+"""
+\t - використовується, для табуляції (як Tab)
+
+
+Всі інші застосовані тут команди описані в Завданні 1
+"""
+
+print('\033[93m"Life is what happens\n\twhen\n\t\tyou`re busy making other plans"\n', '\t' * 9,'\033[0mJohn Lennon')
